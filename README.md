ec8988a8e16e0c615fd13406e1b8caf4c78d019f
vibhumbhardwaj <vibhumbhardwaj@hotmail.com>
2017-08-09 12:46:50 +0530

    changelog

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-# ilabcollab
-Current working url: tiny.cc/ilabcollab
+# ilabcollab -- Version 6
+Current working url: http://tiny.cc/ilabcollab
 
 
-Upcoming:
+***Upcoming***:
 1. persistent data between server restarts. <--done in V6
 2. Cards implementation for collab space. (along with realtime sharing and MEMEs)
 3. One Page for all the rooms (No reloads for room change)
 4. Timestamp for messages.
 5. Different DB for development.
 
-version 6 -->
+**version 6** -->
 1. Messages' persistence now resistant to server restarts !! WOW.
 2. Dropdown available for room login screen
 
-version 5 -->
+**version 5** -->
 1. Spaces in chat room's names are now allowed
 
-version 4.1 -->
+**version 4.1** -->
 1. First working version. changed chat keyword to collab.
 2. new room names with spaces crashes the system :(
--- server.js
+++ server.js
@@ -5,12 +5,13 @@ var morgan = require('morgan');
 var session = require('express-session')({secret:"oye"});
 var cookieParser = require('cookie-parser');
 var server = require('http').Server(app);
-
+var markupRead = require('express-markdown');
 
 var io = require('./socketIOServer.js')(server);
 
+app.use(markupRead({directory: __dirname}));
 
-app.set('views', __dirname + '/web');
+app.set('views', [__dirname + '/web', __dirname]);
 app.set('view engine', 'ejs');
 app.engine('html',require('ejs').renderFile);
 
@@ -33,6 +34,10 @@ var apiRouterSecured = require('./apiRouterSecured.js');
 app.get('/', function(req, res){
     res.render('chatLogin.html');
 })
+app.get('/changelog', (req, res) =>{
+    res.redirect('/README.md');
+})
+
 app.use('/site',viewRouter);
 app.use('/site/gateway', apiRouter);
 app.use('/site/gateway/secure', apiRouterSecured);
