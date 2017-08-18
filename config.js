24bc29f1cdd7e828e5d83a048b8b5503c34d1b9a
vibhumbhardwaj <vibhumbhardwaj@hotmail.com>
2017-08-18 00:28:02 -0400

    fixed untracked files

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    connectionString: process.env.connectionString,
-    mongoIdRegex: /^[A-Za-z0-9]{24}$/,
-    alphaNumericRegex: /^[a-zA-Z0-9_]{1,}$/,
-    searchQueryRegex: /^[a-zA-z0-9 _@#]{1,}$/,
-    secretKey : process.env.secretKey,
-    ImageAPIKey: process.env.azureKey
-}
